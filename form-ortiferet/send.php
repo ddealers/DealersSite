0fc814ccb2cbb071502b2732c02ae9233369e52c
vampaynani <vampay@digitaldealers.mx>
2015-07-24 00:28:28 -0500

    form-ortiferet send

--- form-ortiferet/send.php
+++
@@ -1,7 +0,0 @@
-<?php
-$message = "Nombre: ". $_POST['name'] . "\r\n";
-$message .= "Teléfono: ". $_POST['number'] . "\r\n";
-$message = "Email: ". $_POST['mail'] . "\r\n";
-$message = "Comentarios: ". $_POST['comments'] . "\r\n";
-$message = "País de Interés: ". $_POST['country'] . "\r\n";
-mail($_POST['mail'], $_POST['Formulario de Contacto Ortiferet.net'], $message);
