02d1377623b70ebd1a2b50393bad5c8f2ef816cc
vampaynani <vampay@digitaldealers.mx>
2015-07-23 23:58:00 -0500

    added form-ortiferet

---
+++ form-ortiferet/js/min.js
@@ -0,0 +1,35 @@
+$(document).on("ready",
+	function(){
+		$('#formulario').on("submit", function(event) {
+			event.preventDefault ();
+		var name = $("#form-name").val();
+		var number = $("form-number").val();
+		var mail = $("#form-mail").val();
+		var nameIsString = /[A-Za-z\ ]+/.test(name);
+		var numberIsNaN = /[0-9]/.test(number);
+		var mailIsValid = /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm.test(mail);
+
+		if (!name || !nameIsString){
+			$("#form-name").addClass("rojo");
+			$("#form-name").val("");
+			$("#dat-obli").css({display: "block"});
+		}
+		else {
+			$("#dat-obli").css({display: "none"});
+		}
+		if (!mail || !mailIsValid){
+			$("#form-mail").addClass("rojo");
+			$("#form-mail").val("");
+			$(".incorrecto").css({display: "block"});
+			$(".vacio").css({display: "none"});
+			$(".correcto").css({display: "none"});
+			$("#dat-obli").css({display: "block"});
+			return false;
+		}else {
+			$(".correcto").css({display: "block"});
+			$(".vacio").css({display: "none"});
+			$(".incorrecto").css({display: "none"});
+			$("#dat-obli").css({display: "none"});
+		}	
+	});
+});
